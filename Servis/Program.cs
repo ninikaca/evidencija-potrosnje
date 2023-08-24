dfabf995327aac95e4c09409ea3205e7dc6634d9
Katarina Marinković <ninikaca@gmail.com>
2023-08-24 11:40:44 +0200

    create basic solution project

---
+++ Servis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ServiceModel;
+namespace Servis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
