f2cb9888d3b82c5e2b685bc9cc7d528eb6e20809
Katarina Marinković <ninikaca@gmail.com>
2023-08-25 15:59:54 +0200

    implent IService

---
+++ Servis/Service.cs
@@ -0,0 +1,23 @@
+﻿using BazaPodataka;
+using Common;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Xml;
+
+namespace Servis
+{
+    public delegate void SaveDbDelegate(List<Load> loads);
+
+    public class Service : IService
+    {
+        public void LoadSaveDatabase(MemoryStream stream, string filename)
+        {
+           
+        }        
+    }
+}
