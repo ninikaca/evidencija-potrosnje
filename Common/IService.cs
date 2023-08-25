ebc8580e9b43bf9d5f994642fefda036fbe8ee1a
Katarina Marinković <ninikaca@gmail.com>
2023-08-25 15:50:07 +0200

    create a wcf connection between client and service

---
+++ Common/IService.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+using System.ServiceModel;
+
+namespace Common
+{
+    [ServiceContract]
+    public interface IService
+    {
+        [OperationContract]
+        void LoadSaveDatabase(MemoryStream stream, string filename);
+    }
+}
