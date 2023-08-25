bc6438389c056fc97cebafec773bf660304909e3
Katarina Marinković <ninikaca@gmail.com>
2023-08-25 15:55:56 +0200

    create a file handler for using files

---
+++ Common/FileHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+
+namespace Common
+{
+    [DataContract]
+    public class FileHandler : IDisposable
+    {
+        [DataMember]
+        public MemoryStream Stream { get; set; }
+
+        [DataMember]
+        public string FileName { get; set; }
+
+        [DataMember]
+        private bool disposed = false;
+
+        
+
+        public void Dispose()
+        {
+           
+        }
+    }
+}
